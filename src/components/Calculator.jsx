3df7353927d8963ef417205898b8145405dcf70f
jenish-ml <jenishpunjiri@gmail.com>
2024-08-28 21:53:48 +0530

    update type

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -8,67 +8,72 @@ import { Button } from '@mui/material';
 
 function Calculator() {
     const [total, setTotal] = React.useState(0);
-    const [amount, setAmount] = React.useState(0);
-    const [rate, setRate] = React.useState(0);
-    const [year, setYear] = React.useState(0);
+    const [amount, setAmount] = React.useState('');
+    const [rate, setRate] = React.useState('');
+    const [year, setYear] = React.useState('');
 
-    const calculate = (e) => {
+    const calculate = () => {
         setTotal((amount * rate * year) / 100);
     }
 
-    const reset = (e) => {
+    const reset = () => {
         setTotal(0);
         setAmount(0);
         setRate(0);
         setYear(0);
     }
 
-    console.log(amount, rate, year);
-    console.log(total);
-  return (
-    <div>
-        <div className="container">
-            <div className="header">
-            <h1>Simple Interest Calculator</h1>
-            <p>Enter the values to calculate simple interest</p>
-            <div className="total">
-                <h1> &#8377; {total}</h1>
+    return (
+        <div>
+            <div className="container">
+                <div className="header">
+                    <h1>Simple Interest Calculator</h1>
+                    <p>Enter the values to calculate simple interest</p>
+                    <div className="total">
+                        <h1> &#8377; {total}</h1>
+                    </div>
+                </div>
+                <div className="calculator">
+                    <FormControl className='FormControls'>
+                        <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-amount"
+                            type="number"
+                            value={amount}
+                            onChange={(e) => setAmount(e.target.value)}
+                            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                            label="Amount"
+                        />
+                    </FormControl>
+                    <FormControl className='FormControls'>
+                        <InputLabel htmlFor="outlined-adornment-rate">Rate</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-rate"
+                            type="number"
+                            value={rate}
+                            onChange={(e) => setRate(e.target.value)}
+                            startAdornment={<InputAdornment position="start">%</InputAdornment>}
+                            label="Rate"
+                        />
+                    </FormControl>
+                    <FormControl className='FormControls'>
+                        <InputLabel htmlFor="outlined-adornment-year">Year</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-year"
+                            type="number"
+                            value={year}
+                            onChange={(e) => setYear(e.target.value)}
+                            label="Year"
+                        />
+                    </FormControl>
+                    <div className="btns">
+                        <Button variant="contained" onClick={calculate}>Calculate</Button>
+                        <Button variant="contained" onClick={reset}>Reset</Button>
+                    </div>
+                </div>
             </div>
-            </div>
-            <div className="calculator">
-            <FormControl className='FormControls'>
-            <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
-            <OutlinedInput
-                id="outlined-adornment-amount" onChange={(e) => setAmount(e.target.value)} value={amount||''}
-                startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                label="Amount"
-            />
-            </FormControl>
-            <FormControl className='FormControls'>
-            <InputLabel htmlFor="outlined-adornment-amount">Rate</InputLabel>
-            <OutlinedInput
-                id="outlined-adornment-amount" onChange={(e) => setRate(e.target.value)} value={rate||''}
-                startAdornment={<InputAdornment position="start">%</InputAdornment>}
-                label="Rate"
-            />
-            </FormControl>
-            <FormControl className='FormControls'>
-            <InputLabel htmlFor="outlined-adornment-amount">Year</InputLabel>
-            <OutlinedInput
-                id="outlined-adornment-amount" onChange={(e)=>setYear(e.target.value)} value={year||''} 
-                startAdornment={<InputAdornment position="start"></InputAdornment>}
-                label="Year"
-            />
-            </FormControl>
-            <div className="btns">
-            <Button variant="contained" onClick={calculate}>Calculate</Button>
-            <Button variant="contained" onClick={reset}>Reset</Button>
-            </div>
-            </div>
-
         </div>
-    </div>
     )
-    }
+}
 
-export default Calculator
+export default Calculator;
